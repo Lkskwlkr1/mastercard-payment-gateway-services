d447eeececc19364b11fed5ddaeb3e8ccbaf5017
Long Zhao <zlong@outlook.com>
2019-09-26 12:09:17 +1000

    Updates

---
+++ precache-manifest.4ed8eeed7b4df265137d05e6e545185f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d2c2c127c9877073c01acea21e93aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4650239332e5bb065b3a",
+    "url": "/static/css/2.0b0d73e4.chunk.css"
+  },
+  {
+    "revision": "8a27f0527c99ea9d6da4",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4650239332e5bb065b3a",
+    "url": "/static/js/2.b223ec70.chunk.js"
+  },
+  {
+    "revision": "8a27f0527c99ea9d6da4",
+    "url": "/static/js/main.a70818ec.chunk.js"
+  },
+  {
+    "revision": "0aa09a1a4b33598c76be",
+    "url": "/static/js/runtime-main.85302524.js"
+  },
+  {
+    "revision": "0f3639caf9d1a6aae2385ad72b1a0b83",
+    "url": "/static/media/CiutadellaMedium.0f3639ca.woff"
+  },
+  {
+    "revision": "3d4de8749e08ab6e41a1b6437355e5d4",
+    "url": "/static/media/CiutadellaMedium.3d4de874.svg"
+  },
+  {
+    "revision": "9fb0515b64dfae626b84c935de89aea5",
+    "url": "/static/media/CiutadellaMedium.9fb0515b.woff2"
+  },
+  {
+    "revision": "a72af672a4b8905d2a2bf25fead84960",
+    "url": "/static/media/CiutadellaMedium.a72af672.ttf"
+  },
+  {
+    "revision": "b751a2e31ab12f0e0128d9d6289b9c29",
+    "url": "/static/media/CiutadellaRegular.b751a2e3.svg"
+  },
+  {
+    "revision": "b7685a0f3b9a296df919b11c7a832c12",
+    "url": "/static/media/CiutadellaRegular.b7685a0f.ttf"
+  },
+  {
+    "revision": "b829f6e40dbb9fc8c68f8f3d45065ba8",
+    "url": "/static/media/CiutadellaRegular.b829f6e4.woff2"
+  },
+  {
+    "revision": "d343acf7cabaf2e89552eafb35ce578f",
+    "url": "/static/media/CiutadellaRegular.d343acf7.woff"
+  }
+]);
